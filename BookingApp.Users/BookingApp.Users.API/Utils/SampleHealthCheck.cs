4b5a7faeacd9c22dc162eb187260720a132f70ed
g.gerdjikov <g.gerdjikov@outlook.com>
2022-08-16 17:27:12 +0300

    Add sample health-checks

---
+++ BookingApp.Users/BookingApp.Users.API/Utils/SampleHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BookingApp.Users.API.Utils
+{
+    public class SampleHealthCheck : IHealthCheck
+    {
+        private static readonly Random _rnd = new Random();
+
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var isHealthy = true;
+
+            //logic to determine if the healthCheck should be true
+            isHealthy = _rnd.Next(5) == 0;
+
+            if (isHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                new HealthCheckResult(
+                    context.Registration.FailureStatus, "An unhealthy result."));
+        }
+    }
+}
